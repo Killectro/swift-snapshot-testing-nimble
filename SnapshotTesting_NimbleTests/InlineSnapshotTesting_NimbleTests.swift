066bb1ff1d43e5195960ccb10904249bd06853e0
lukedurrant <contact@lukedurrant.com>
2019-04-29 16:06:23 +1000

    tests

---
+++ SnapshotTesting_NimbleTests/InlineSnapshotTesting_NimbleTests.swift
@@ -0,0 +1,57 @@
+//
+//  SnapshotTesting_Nimble_ExampleTests.swift
+//  SnapshotTesting-Nimble_ExampleTests
+//
+//  Created by DJ Mitchell on 12/4/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+import SnapshotTesting
+import SnapshotTesting_Nimble
+
+struct TestViewModel {
+    let itemId: Int
+    let exampleBool: Bool
+}
+
+class InlineSnapshotTestingNimbleSpec: QuickSpec {
+
+    override func spec() {
+        describe("in arbitrary context") {
+
+            describe("view recursive description") {
+                var viewModel: TestViewModel!
+
+                beforeEach {
+                    viewModel = TestViewModel(itemId: 123, exampleBool: true)
+                }
+
+                it("has valid snapshot") {
+                    expect(viewModel).to(haveValidInlineSnapshot(as: .dump, with: """
+                    ▿ TestViewModel
+                      - exampleBool: true
+                      - itemId: 123
+                    """))
+                }
+
+                it("has valid snapshot named") {
+                    expect(viewModel).to(haveValidInlineSnapshot(as: .dump, named: "some name", with: """
+                    ▿ TestViewModel
+                      - exampleBool: true
+                      - itemId: 123
+                    """))
+                }
+
+                it("has valid snapshot testNamed") {
+                    expect(viewModel).to(haveValidInlineSnapshot(as: .dump, with: """
+                    ▿ TestViewModel
+                      - exampleBool: true
+                      - itemId: 123
+                    """, testName: "valid_recursive_snapshot"))
+                }
+            }
+        }
+    }
+}
