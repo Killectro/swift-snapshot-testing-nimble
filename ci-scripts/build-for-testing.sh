1cc9551d3922892ed032841a9995dc41caa4d832
Kemal Enver <kemal.enver@domain.com.au>
2021-06-03 09:43:23 +1000

    Update test device

--- ci-scripts/build-for-testing.sh
+++ ci-scripts/build-for-testing.sh
@@ -5,6 +5,6 @@ xcodebuild \
   	-project SnapshotTesting-Nimble.xcodeproj \
   	-scheme SnapshotTesting-Nimble \
   	-sdk iphonesimulator \
-  	-destination 'platform=iOS Simulator,name=iPhone 11,OS=14.2' \
+  	-destination 'platform=iOS Simulator,name=iPhone 11,OS=14.5' \
   	-derivedDataPath 'build'
   
--- ci-scripts/test-without-building.sh
+++ ci-scripts/test-without-building.sh
@@ -2,6 +2,6 @@
 
 xcodebuild \
 	test-without-building \
-	-xctestrun 'build/Build/Products/SnapshotTesting-Nimble_iphonesimulator14.2-x86_64.xctestrun' \
-  	-destination 'platform=iOS Simulator,name=iPhone 11,OS=14.2'
+	-xctestrun 'build/Build/Products/SnapshotTesting-Nimble_iphonesimulator14.5-x86_64.xctestrun' \
+  	-destination 'platform=iOS Simulator,name=iPhone 11,OS=14.5'
   
